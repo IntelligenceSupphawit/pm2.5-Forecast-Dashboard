42834f3a3ee72814ab9f19f0e238b212efdb3208
Kittisak Keawpichai <6710110030@psu.ac.th>
2025-03-16 23:07:45 +0700

    pm10

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import plotly.express as px
 exp = RegressionExperiment()
 pm10_model = exp.load_model('pm10_nofeture')
 
+
 # เตรียมข้อมูลสำหรับทำนาย
 future = pd.DataFrame()
 start_date = pd.Timestamp.now()
@@ -20,22 +21,49 @@ future['humidity'] = [50, 55, 60, 65, 70, 75, 80]  # ตัวอย่างค
 future['temperature'] = [25, 26, 27, 28, 29, 30, 31]  # ตัวอย่างค่า temperature
 future['pm10'] = exp.predict_model(pm10_model, data=future)['prediction_label']  # ทำนายค่า PM10
 
-# สร้างฮิสโตแกรม
+today_temperature = future['temperature'].iloc[0]
+today_humidity = future['humidity'].iloc[0]
+
+# สร้างกราฟแท่ง
 fig = px.bar(future, x='day', y='pm10', title='PM10 Levels Over 7 Days', labels={'day': 'Day', 'pm10': 'PM10 Value'})
 
 # Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container([
     dbc.Row(
-        dbc.Col(html.H1("PM10 7-Day Forecast"), width=12)
+        dbc.Col(html.H1("PM 2.5 Forecast"), width=12)
     ),
-    dbc.Row(
+    html.Hr(),
+    # dbc.Row( # pm 2.5 เลือกที่ได้
+    #     dbc.Col(html.Pre(future[['date', 'pm10']].to_string(index=False), className="border p-3"), width=12)
+    # ),
+    dbc.Row([
+        dbc.Col(dbc.Card([
+            dbc.CardBody([
+                html.H4("Today's Temperature", className="card-title text-white"),
+                html.P(f"{today_temperature}°C", className="card-text text-white")
+            ])
+        ], className="mb-3 bg-dark text-center", style={"width": "150px", "height": "150px"}), width="auto"),
+        dbc.Col(dbc.Card([
+            dbc.CardBody([
+                html.H4("Today's Humidity", className="card-title text-white"),
+                html.P(f"{today_humidity}%", className="card-text text-white")
+            ])
+        ], className="mb-3 bg-dark text-center", style={"width": "150px", "height": "150px"}), width="auto")
+    ], justify="center"),
+    
+    dbc.Row( #ตารางทำนาย
         dbc.Col(html.Pre(future[['date', 'pm10']].to_string(index=False), className="border p-3"), width=12)
     ),
     dbc.Row(
-        dbc.Col(dcc.Graph(figure=fig), width=12)  # เพิ่มกราฟฮิสโตแกรม
+        dbc.Col(dbc.Card([
+            dbc.CardBody([
+                dcc.Graph(figure=fig)
+            ])
+        ]), width=12)
     )
 ], fluid=True)
+    
 
 # Run the application
 if __name__ == '__main__':
